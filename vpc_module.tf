59d30e7f75190567e62bb308bc8c1ce1dbef8b33
cameron <cmonthe8@outlook.com>
2022-05-23 15:37:05 -0400

    vpc_updated

--- vpc_module.tf
+++ vpc_module.tf
@@ -4,7 +4,7 @@ module "vpc" {
   name = "cameron-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-1a",]
+  azs             = ["us-east-1",]
   private_subnets = ["10.0.1.0/24",]
   public_subnets  = ["10.0.101.0/24",]
 
