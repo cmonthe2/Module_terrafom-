b9ce25470c5ddc483837f8c12796660b0d41ad3d
cmonthe2 <88361315+cmonthe2@users.noreply.github.com>
2022-05-23 15:58:31 -0400

    Update vpc_module.tf

--- vpc_module.tf
+++ vpc_module.tf
@@ -3,11 +3,7 @@ module "vpc" {
 
   name = "cameron-vpc"
   cidr = "10.0.0.0/16"
-
-  azs             = ["us-east-1",]
-  private_subnets = ["10.0.1.0/24",]
-  public_subnets  = ["10.0.101.0/24",]
-
+  
   enable_nat_gateway = true
   enable_vpn_gateway = true
 
